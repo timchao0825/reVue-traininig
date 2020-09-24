52f5d74d0692a68360753849aa1cd6692cbab73a
Tim <tim.chao0825@gmail.com>
2020-09-24 19:29:59 +0800

    start project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <Letconst />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import Letconst from "./components/es6-review/letconst.vue";
+export default {
+  name: "App",
+  components: {
+    // HelloWorld,
+    Letconst,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/es6-review/letconst.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Let Const</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "letconst",
+};
+
+console.log("vue let const");
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: red;
+}
+</style>
