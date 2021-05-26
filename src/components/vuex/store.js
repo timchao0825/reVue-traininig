a7da25e0c173fba3e65d94a5311580cff32014c2
timchao <timchao@cw.com.tw>
2021-05-26 23:01:50 +0800

    vuex training

--- src/components/vuex/store.js
+++ src/components/vuex/store.js
@@ -7,10 +7,15 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
     count: 0,
+    username: '',
+    list: [],
   },
+  // mutations 只能進行同步操作，非同步處理使用actions
   mutations: {
-    addCount(state) {
-      state.count += 1
+    addCount(state, payLoad) {
+      state.count += payLoad
+      // store commit 寫成物件的方式 指定step
+      // state.count += payLoad.step
     },
   },
 })
--- src/components/vuex/vuexIndex.vue
+++ src/components/vuex/vuexIndex.vue
@@ -6,21 +6,47 @@
 </template>
 
 <script>
+import { mapMutations, mapState } from 'vuex'
+
 export default {
   name: 'vuexIndex',
-  data() {
-    return {}
-  },
+  // 另外可以多寫一些computed function 的用法
   computed: {
-    count() {
-      return this.$store.state.count
-    },
+    ...mapState(['count']),
+    // ...mapState 也可以是物件
+    // ...mapState({
+    //  storeCount : 'count', template的 {{count}} 可以更名為storeCount
+    // 如果指定成函式(可經過一些處理)
+    // stoureCount(state){
+    //   return state.count
+    // }
+    // })
   },
+  // 單一呼叫vuex state 的用法
+  // computed: mapState([
+  //   'count',
+  //   'username',
+  //   'list',
+  // ]),
+  // 另外可以多寫 methods 的呼叫vuex mutations 的用法
   methods: {
-    addCount() {
-      this.$store.commit('addCount')
-    },
+    ...mapMutations(['addCount']),
   },
+
+  // methods: mapMutations({
+  //   // 可以更名為add 上放的click事件需寫成add
+  //   add: 'addCount',
+  // }),
+  // methods: {
+  //   addCount() {
+  //     // this.$store.commit('addCount', 2)
+  //     // 傳入物件的寫法
+  //     this.$store.commit({
+  //       type: 'addCount',
+  //       step: 2,
+  //     })
+  //   },
+  // },
 }
 </script>
 
